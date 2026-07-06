bc760c19c75008f5f342234cfcbe908ccd5ce82b
Emre <emre.sahiner@workintech.com.tr>
2026-07-06 20:38:42 +0300

    feat: add left nav menu -basic version

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,5 +1,14 @@
+import { menu } from "../utils";
+import MenuItem from "./MenuItem";
+
 function LeftSidebar() {
-  return <div className="w-1/6">LeftSidebar</div>;
+  return (
+    <div className="w-1/6 pt-8">
+      {menu.map((item, index) => (
+        <MenuItem key={index} item={item} />
+      ))}
+    </div>
+  );
 }
 
 export default LeftSidebar;
